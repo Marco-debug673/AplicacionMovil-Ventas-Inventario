36588b6514fc5a9e2fc1bc0e28e26044d5736cf8
Marco-debug673 <anthony_uchiha_99@hotmail.com>
2026-05-02 15:06:22 -0600

    Se paso de modo de prueba a modo de produccion la base de datos

--- Gestion_Agil/app/src/main/java/com/example/gestion_agil/data/model/AppDatabase.kt
+++ Gestion_Agil/app/src/main/java/com/example/gestion_agil/data/model/AppDatabase.kt
@@ -10,7 +10,7 @@ import androidx.room.RoomDatabase
     Ventas::class,
     Detalle_Venta::class,
     Notificacion::class,
-    Sesion::class], version = 6, exportSchema = false)
+    Sesion::class], version = 6, exportSchema = true)
 
 abstract class AppDatabase : RoomDatabase() {
 
@@ -32,7 +32,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "Tienda_db"
                 )
-                    .fallbackToDestructiveMigration(true)
+                    // .fallbackToDestructiveMigration() // Eliminado para producción para evitar pérdida de datos
                     .build()
                 INSTANCE = instance
                 instance
